2810760ee4073f39faa5dc98b86492ede01df403
pKarpa <pavlo.karpa17@gmail.com>
2021-11-08 20:33:51 +0200

    Create Database

---
+++ SomeWebApp/SomeWebApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using SomeWebApp.Models.Data;
+
+namespace SomeWebApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<News> NewsRepo { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<BlockedUser> BlockedUsers { get; set; }
+    }
+}
